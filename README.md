b1fc4ebfff7e37bbcded14e40a81fbb429dcadd6
ban <jim515jim@gmail.com>
2022-06-17 16:26:54 +0800

    微調程式 順便添加上使用說明

---
+++ README.md
@@ -0,0 +1,13 @@
+# About This repository (關於這個 repository)
+
+- This is abut get Taiwan's stock data.(這是一個關於取得台灣每日個股盤後資訊的程式碼。)
+- This is a simple program.(這是一個簡單的程式碼。)
+
+## How to use (怎麼使用)
+
+```
+git clone https://github.com/jim515jim/stock.git
+cd stock
+coposer install
+php stock.php
+```
--- getstock.php
+++ getstock.php
@@ -1,22 +1,30 @@
 <?php
 require 'vendor/autoload.php';
-require 'config.php';
-use GuzzleHttp\Pool;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
+
+use GuzzleHttp\Exception\GuzzleException;
 use Simplon\Mysql\Mysql;
 use Simplon\Mysql\PDOConnector;
 
-$pdoConnector = new PDOConnector($config['server'], $config['username'], $config['password'], $config['database']);
-$dbh = new Mysql($pdoConnector->connect());
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
 
-exit;
-
-$client = new GuzzleHttp\Client(['base_uri' => 'http://www.twse.com.tw']);
-$response = $client->request('GET', '/exchangeReport/MI_INDEX?response=json&date=20180503&type=ALL&_='.round(microtime(true) * 1000));
-$text = json_decode($response->getBody()->getContents());
-
-foreach($text->data5 as $k => $v){
-    print_r($v);
-    echo $v[0];
+$pdoConnector = new PDOConnector($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
+try {
+	$dbh = new Mysql($pdoConnector->connect());
+} catch (Exception $e) {
+	echo $e->getMessage();
 }
+
+try {
+	$client = new GuzzleHttp\Client(['base_uri' => 'https://www.twse.com.tw']);
+	$response = $client->request('GET', '/exchangeReport/MI_INDEX?response=json&date=20180503&type=ALL&_=' . round(microtime(true) * 1000));
+
+	$text = json_decode($response->getBody()->getContents());
+	foreach($text->data5 as $k => $v){
+		print_r($v);
+	}
+} catch (GuzzleException $e) {
+	echo $e->getMessage();
+}
+
+
